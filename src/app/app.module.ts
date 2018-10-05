03fa38e18f862bef4785f82e0e22a98aecb1512c
SRAVAN KUMAR NANGUNOORI <nangunoori.sravan@gmail.com>
2018-10-05 19:45:05 -0400

    addition of routes and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BloodBankComponent } from './blood-bank/blood-bank.component';
+import { HospitalComponent } from './hospital/hospital.component';
+import { UserComponent } from './user/user.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [  
+  { path: 'bloodBank', component:BloodBankComponent  },
+  
+  { path: 'user', component:UserComponent  },
+  
+  { path: 'hospital', component:HospitalComponent  },
+  
+  { path: '', redirectTo: '/user', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BloodBankComponent,
+    HospitalComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
