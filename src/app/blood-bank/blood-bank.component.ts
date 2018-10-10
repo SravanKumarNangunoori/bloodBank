b640417a89c5963ef9ac8fc35e5092292f7e5ea3
sumanlataghosh <37414436+sumanlataghosh@users.noreply.github.com>
2018-10-10 13:32:57 -0400

    Update blood-bank.component.ts

--- src/app/blood-bank/blood-bank.component.ts
+++ src/app/blood-bank/blood-bank.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RestClientService} from '../rest.client.service';
+import { HttpHeaderResponse } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule, JsonpModule } from '@angular/http';
+
+
 
 @Component({
   selector: 'app-blood-bank',
@@ -7,9 +14,123 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BloodBankComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private restclient: RestClientService) { }
+  mockData: any;
+  getdata: any;
+  firstdata:string;
+  showForm:boolean;
+  bloodbanks:any;
+  bloodbank:any;
+  activebloodbankId:any;
+  showbloodbankDetails:boolean;
+  
+ ngOnInit() {
+    this.mockData = { "Name": "Albany blood bank","Address":"Albany","Apos":30,"Bpos":20,"ABpos":40,"Opos":25,"Aminus":30,"Bminus":20,"ABminus":40,"Ominus":25}
+    this.showForm = false;
+    this.showbloodbankDetails = false;
   }
 
+  selectChange(event:any){
+    this.showbloodbank(event.target.value);
+  }
+  selectDetailChange(event:any){
+    for(var i = 0; i < this.bloodbanks.length; i++){
+      if(this.bloodbanks[i]._id == event.target.value){
+        this.activebloodbankId = event.target.value;
+        return;
+      }
+    }
+  }
+
+  showbloodbank(id:any){
+    this.showbloodbankDetails = false;
+    for(var i = 0; i < this.bloodbanks.length; i++){
+      if(this.bloodbanks[i]._id == id){
+        this.bloodbank = this.bloodbanks[i];
+        console.log("in showbloodbank")
+        //console.log(this.bloodbank);
+        return;
+      }
+    }
+  }
+// Update bloodbank data
+onFormSubmit(event){
+  var postadata = this.serializeForm(event.target);
+  this.restclient.post('api/updatebloodbank', postadata).subscribe((result)=>{
+    console.log(result);
+  }, (error)=>{
+    console.log(error);
+  })
 }
+serializeForm(form) {
+  var obj = {};
+  var elements = form.querySelectorAll( "input, select, textarea" );
+  for( var i = 0; i < elements.length; ++i ) {
+    var element = elements[i];
+    var name = element.name;
+    var value = element.value;
+
+    if( name ) {
+      obj[ name ] = value;
+    }
+  }
+
+  return obj;
+}
+//get bloodbank data
+getbloodbankdata(){
+  console.log("bloodbank");
+  var that = this;
+  this.restclient.get('api/bloodbankcollection').subscribe(
+    (result) => {
+      
+      that.bloodbanks = result;
+      that.showbloodbank(result[0]._id);
+      that.showForm = true;
+      that.showbloodbankDetails = false;
+    }, (error) => {
+      console.log(error);
+    }
+  )
+}
+
+showbloodbankdata(){
+  var that = this;
+  this.restclient.get('api/bloodbankcollection').subscribe(
+    (result) => {
+      console.log("in show para");
+      that.bloodbanks = result;
+      that.showbloodbankDetails = true;
+      that.showForm = false;
+      that.activebloodbankId = result[0]._id;
+      console.log(result[0]._id)
+    }, (error) => {
+      console.log(error);
+    }
+  )
+}
+
+
+//post bloodbank data
+postbloodbankdata() {
+  console.log("bloodbank post");
+  this.restclient.post('api/postbloodbank',this.mockData).subscribe(
+    (result) => {
+      console.log("data is",result);
+
+    }
+    ,(error) => {
+      console.log(error)
+    }
+  )
+}
+deleteall(){
+  this.restclient.post('api/deleteall', {}).subscribe((result)=>{
+    console.log(result);
+  }, (error)=>{
+    console.log(error);
+  })
+}
+}
+
+
