a670b98dd5f19f962f772676c0c497318a2023c2
prasanna96227 <lakshmiprasanna96227@gmail.com>
2018-11-13 21:04:21 +0530

    UI prasanna

--- src/app/blood-bank/blood-bank.component.ts
+++ src/app/blood-bank/blood-bank.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RestClientService} from '../rest.client.service';
+import { RestClientService } from '../rest.client.service';
 import { HttpHeaderResponse } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -8,143 +8,149 @@ import { HttpModule, JsonpModule } from '@angular/http';
 
 
 @Component({
-  selector: 'app-blood-bank',
-  templateUrl: './blood-bank.component.html',
-  styleUrls: ['./blood-bank.component.scss']
+    selector: 'app-blood-bank',
+    templateUrl: './blood-bank.component.html',
+    styleUrls: ['./blood-bank.component.scss']
 })
 export class BloodBankComponent implements OnInit {
 
-  constructor(private restclient: RestClientService) { }
-  mockData: any;
-  getdata: any;
-  firstdata:string;
-  showForm:boolean;
-  bloodbanks:any;
-  bloodbank:any;
-  activebloodbankId:any;
-  showbloodbankDetails:boolean;
-  
- ngOnInit() {
-    this.mockData = { "Name": "Albany blood bank","Address":"Albany","Apos":30,"Bpos":20,"ABpos":40,"Opos":25,"Aminus":30,"Bminus":20,"ABminus":40,"Ominus":25}
-    this.showForm = false;
-    this.showbloodbankDetails = false;
-    this.showbloodbankdata();
-  }
+    constructor(private restclient: RestClientService) { }
+    mockData: any;
+    getdata: any;
+    firstdata: string;
+    showForm: boolean;
+    bloodbanks: any;
+    bloodbank: any;
+    activebloodbankId: any;
+    showbloodbankDetails: boolean;
+    shwForm: boolean = true;
+    sho: boolean = true;
 
-  selectChange(event:any){
-   // this.showbloodbank(event.target.value);
-  }
-  selectDetailChange(event:any){
-    for(var i = 0; i < this.bloodbanks.length; i++){
-      if(this.bloodbanks[i]._id == event.target.value){
-        this.activebloodbankId = event.target.value;
-        return;
-      }
+    ngOnInit() {
+        this.mockData = { "Name": "Albany blood bank", "Address": "Albany", "Apos": 30, "Bpos": 20, "ABpos": 40, "Opos": 25, "Aminus": 30, "Bminus": 20, "ABminus": 40, "Ominus": 25 }
+        this.showForm = false;
+        this.showbloodbankDetails = false;
+        this.showbloodbankdata();
     }
-  }
 
-  showbloodbank(){
-    this.showbloodbankDetails = false;
-    let email = (localStorage.getItem('userid')); 
-    for(var i = 0; i < this.bloodbanks.length; i++){
-      if(this.bloodbanks[i].email == email){
-        this.bloodbank = this.bloodbanks[i];
-        console.log("in showbloodbank")
-        //console.log(this.bloodbank);
-        return;
-      }
+    selectChange(event: any) {
+        // this.showbloodbank(event.target.value);
     }
-  }
-// Update bloodbank data
-onFormSubmit(event){
-  var postadata = this.bloodbank //this.serializeForm(event.target);
-  this.restclient.post('api/updatebloodbank', postadata).subscribe((result)=>{
-    if(result){
-      this.showbloodbankdata();
+    selectDetailChange(event: any) {
+        for (var i = 0; i < this.bloodbanks.length; i++) {
+            if (this.bloodbanks[i]._id == event.target.value) {
+                this.activebloodbankId = event.target.value;
+                return;
+            }
+        }
     }
-  }, (error)=>{
-    console.log(error);
-  })
-}
-serializeForm(form) {
-  var obj = {};
-  var elements = form.querySelectorAll( "input, select, textarea" );
-  for( var i = 0; i < elements.length; ++i ) {
-    var element = elements[i];
-    var name = element.name;
-    var value = element.value;
 
-    if( name ) {
-      obj[ name ] = value;
+    showbloodbank() {
+        this.showbloodbankDetails = false;
+        let email = (localStorage.getItem('userid'));
+        for (var i = 0; i < this.bloodbanks.length; i++) {
+            if (this.bloodbanks[i].email == email) {
+                this.bloodbank = this.bloodbanks[i];
+                console.log("in showbloodbank")
+                //console.log(this.bloodbank);
+                return;
+            }
+        }
     }
-  }
+    // Update bloodbank data
+    onFormSubmit(event) {
+        var postadata = this.bloodbank //this.serializeForm(event.target);
+        this.restclient.post('api/updatebloodbank', postadata).subscribe((result) => {
+            if (result) {
+                this.showbloodbankdata();
+            }
+        }, (error) => {
+            console.log(error);
+        })
+    }
+    serializeForm(form) {
+        var obj = {};
+        var elements = form.querySelectorAll("input, select, textarea");
+        for (var i = 0; i < elements.length; ++i) {
+            var element = elements[i];
+            var name = element.name;
+            var value = element.value;
 
-  return obj;
-}
-//get bloodbank data
-getbloodbankdata(){
-  console.log("bloodbank");
-  var that = this;
-  this.restclient.get('api/bloodbankcollection').subscribe(
-    (result) => {
-      
-      that.bloodbanks = result;
-      that.showbloodbank();
-      that.showForm = true;
-      that.showbloodbankDetails = false;
-    }, (error) => {
-      console.log(error);
+            if (name) {
+                obj[name] = value;
+            }
+        }
+
+        return obj;
     }
-  )
-}
-getCurrentLoggedInUser() {
-  let email = (localStorage.getItem('userid')); 
-  for (var i = 0; i < this.bloodbanks.length; i++) {
-  if (this.bloodbanks[i].email == email) {
-  this.bloodbank = this.bloodbanks[i]; 
-  this.activebloodbankId = this.bloodbanks[i]._id; 
-  console.log(this.bloodbank); 
-  return; 
-  }
-  }
-  }
-showbloodbankdata(){
-  var that = this;
-  this.restclient.get('api/bloodbankcollection').subscribe(
-    (result) => {
-      console.log("in show para");
-      that.bloodbanks = result;
-      that.showbloodbankDetails = true;
-      that.showForm = false;
-      that.getCurrentLoggedInUser(); 
-      console.log(result[0]._id)
-    }, (error) => {
-      console.log(error);
+    //get bloodbank data
+    getbloodbankdata() {
+        console.log("bloodbank");
+        var that = this;
+        this.restclient.get('api/bloodbankcollection').subscribe(
+            (result) => {
+
+                that.bloodbanks = result;
+                that.showbloodbank();
+                that.showForm = this.sho;
+                that.shwForm = false;
+                that.showbloodbankDetails = false;
+            }, (error) => {
+                console.log(error);
+            }
+        )
     }
-  )
-}
+    new() {
+        this.shwForm = true;
+    }
+    getCurrentLoggedInUser() {
+        let email = (localStorage.getItem('userid'));
+        for (var i = 0; i < this.bloodbanks.length; i++) {
+            if (this.bloodbanks[i].email == email) {
+                this.bloodbank = this.bloodbanks[i];
+                this.activebloodbankId = this.bloodbanks[i]._id;
+                console.log(this.bloodbank);
+                return;
+            }
+        }
+    }
+    showbloodbankdata() {
+        var that = this;
+        this.restclient.get('api/bloodbankcollection').subscribe(
+            (result) => {
+                console.log("in show para");
+                that.bloodbanks = result;
+                that.showbloodbankDetails = true;
+                that.showForm = false;
+                that.getCurrentLoggedInUser();
+                console.log(result[0]._id)
+            }, (error) => {
+                console.log(error);
+            }
+        )
+    }
 
 
-//post bloodbank data
-postbloodbankdata() {
-  console.log("bloodbank post");
-  this.restclient.post('api/postbloodbank',this.mockData).subscribe(
-    (result) => {
-      console.log("data is",result);
+    //post bloodbank data
+    postbloodbankdata() {
+        console.log("bloodbank post");
+        this.restclient.post('api/postbloodbank', this.mockData).subscribe(
+            (result) => {
+                console.log("data is", result);
 
+            }
+            , (error) => {
+                console.log(error)
+            }
+        )
     }
-    ,(error) => {
-      console.log(error)
+    deleteall() {
+        this.restclient.post('api/deleteall', {}).subscribe((result) => {
+            console.log(result);
+        }, (error) => {
+            console.log(error);
+        })
     }
-  )
-}
-deleteall(){
-  this.restclient.post('api/deleteall', {}).subscribe((result)=>{
-    console.log(result);
-  }, (error)=>{
-    console.log(error);
-  })
-}
 }
 
 
--- src/app/hospital/hospital.component.ts
+++ src/app/hospital/hospital.component.ts
@@ -1,144 +1,151 @@
-import {Component, OnInit }from '@angular/core'; 
-import {RestClientService}from '../rest.client.service'; 
-import {HttpHeaderResponse }from '@angular/common/http'; 
-import {BrowserModule }from '@angular/platform-browser'; 
-import {NgModule }from '@angular/core'; 
-import {HttpModule, JsonpModule }from '@angular/http'; 
+import { Component, OnInit } from '@angular/core';
+import { RestClientService } from '../rest.client.service';
+import { HttpHeaderResponse } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 
-@Component( {
-selector:'app-hospital', 
-templateUrl:'./hospital.component.html', 
-styleUrls:['./hospital.component.scss']
+@Component({
+    selector: 'app-hospital',
+    templateUrl: './hospital.component.html',
+    styleUrls: ['./hospital.component.scss']
 
 })
 export class HospitalComponent implements OnInit {
 
-constructor(private restclient:RestClientService) {}
-mockData:any; 
-getdata:any; 
-firstdata:string; 
-showForm:boolean; 
-hospitals:any; 
-hospital:any; 
-activeHospitalId:any; 
-showHospitalDetails:boolean; 
+    constructor(private restclient: RestClientService) { }
+    mockData: any;
+    getdata: any;
+    firstdata: string;
+    showForm: boolean;
+    hospitals: any;
+    hospital: any;
+    activeHospitalId: any;
+    showHospitalDetails: boolean;
+    sho: boolean = true;
+    shwForm: boolean;
 
-ngOnInit() {
-//this.mockData = { "Name": "Albany HOSPITAL","Address":"Albany","Apos":30,"Bpos":20,"ABpos":40,"Opos":25,"Aminus":30,"Bminus":20,"ABminus":40,"Ominus":25}
-this.showForm = false; 
-this.showHospitalDetails = false; 
-this.showhospitaldata(); 
-}
 
-selectChange(event:any) {
-//this.showHospital(event.target.value);
-}
-selectDetailChange(event:any) {
-for (var i = 0; i < this.hospitals.length; i++) {
-if (this.hospitals[i]._id == event.target.value) {
-this.activeHospitalId = event.target.value; 
-return; 
-}
-}
-}
+    ngOnInit() {
+        //this.mockData = { "Name": "Albany HOSPITAL","Address":"Albany","Apos":30,"Bpos":20,"ABpos":40,"Opos":25,"Aminus":30,"Bminus":20,"ABminus":40,"Ominus":25}
+        this.showForm = false;
+        this.showHospitalDetails = false;
+        this.showhospitaldata();
+    }
 
-showHospital() {
-this.showHospitalDetails = false; 
-let email = (localStorage.getItem('userid')); 
-for (var i = 0; i < this.hospitals.length; i++) {
-if (this.hospitals[i].email == email) {
-this.hospital = this.hospitals[i]; 
-this.activeHospitalId = this.hospitals[i]._id; 
-console.log(this.hospital); 
-return; 
-}
-}
-}
-// Update hospital data
-onFormSubmit(event) {
-var postadata = this.hospital; 
-this.restclient.post('api/updatehospital', postadata).subscribe((result) =>  {
-if (result) {this.showhospitaldata(); 
-}
-}, (error) =>  {
-console.log(error); 
-})
-}
-serializeForm(form) {
-var obj =  {}; 
-var elements = form.querySelectorAll("input, select, textarea"); 
-for (var i = 0; i < elements.length; ++i) {
-var element = elements[i]; 
-var name = element.name; 
-var value = element.value; 
+    selectChange(event: any) {
+        //this.showHospital(event.target.value);
+    }
+    selectDetailChange(event: any) {
+        for (var i = 0; i < this.hospitals.length; i++) {
+            if (this.hospitals[i]._id == event.target.value) {
+                this.activeHospitalId = event.target.value;
+                return;
+            }
+        }
+    }
 
-if (name) {
-obj[ name ] = value; 
-}
-}
+    showHospital() {
+        this.showHospitalDetails = false;
+        let email = (localStorage.getItem('userid'));
+        for (var i = 0; i < this.hospitals.length; i++) {
+            if (this.hospitals[i].email == email) {
+                this.hospital = this.hospitals[i];
+                this.activeHospitalId = this.hospitals[i]._id;
+                console.log(this.hospital);
+                return;
+            }
+        }
+    }
+    // Update hospital data
+    onFormSubmit(event) {
+        var postadata = this.hospital;
+        this.restclient.post('api/updatehospital', postadata).subscribe((result) => {
+            if (result) {
+                this.showhospitaldata();
+            }
+        }, (error) => {
+            console.log(error);
+        })
+    }
+    serializeForm(form) {
+        var obj = {};
+        var elements = form.querySelectorAll("input, select, textarea");
+        for (var i = 0; i < elements.length; ++i) {
+            var element = elements[i];
+            var name = element.name;
+            var value = element.value;
 
-return obj; 
-}
-//get hospital data
-gethospitaldata() {
-console.log("hospital"); 
-var that = this; 
-this.restclient.get('api/hospitalcollection').subscribe(
-(result) =>  {
-//var inp1 = document.getElementById("Name");
-//var inp2 = document.getElementById("Address");
-that.hospitals = result; 
-that.showHospital(); 
-that.showForm = true; 
-that.showHospitalDetails = false; 
-}, (error) =>  {
-console.log(error); 
-})
-}
+            if (name) {
+                obj[name] = value;
+            }
+        }
 
-showhospitaldata() {
-var that = this; 
-this.restclient.get('api/hospitalcollection').subscribe(
-(result) =>  {
-that.hospitals = result; 
-that.showHospitalDetails = true; 
-that.showForm = false; 
-that.getCurrentLoggedInUser(); 
-}, (error) =>  {
-console.log(error); 
-})
-}
+        return obj;
+    }
+    //get hospital data
+    gethospitaldata() {
 
-getCurrentLoggedInUser() {
-let email = (localStorage.getItem('userid')); 
-for (var i = 0; i < this.hospitals.length; i++) {
-if (this.hospitals[i].email == email) {
-this.hospital = this.hospitals[i]; 
-this.activeHospitalId = this.hospitals[i]._id; 
-console.log(this.hospital); 
-return; 
-}
-}
-}
+        console.log("hospital");
+        var that = this;
+        this.restclient.get('api/hospitalcollection').subscribe(
+            (result) => {
+                //var inp1 = document.getElementById("Name");
+                //var inp2 = document.getElementById("Address");
+                that.hospitals = result;
+                that.showHospital();
+                that.showForm = this.sho;
+                that.shwForm = !this.sho;
+                that.showHospitalDetails = false;
+            }, (error) => {
+                console.log(error);
+            })
+    }
 
-//post hospital data
-posthospitaldata() {
-console.log("hospital post"); 
-this.restclient.post('api/posthospital', this.mockData).subscribe(
-(result) =>  {
-console.log("data is", result); 
+    showhospitaldata() {
+        var that = this;
+        this.restclient.get('api/hospitalcollection').subscribe(
+            (result) => {
+                that.hospitals = result;
+                that.showHospitalDetails = true;
+                that.showForm = false;
+                this.shwForm = true;
+                that.getCurrentLoggedInUser();
+            }, (error) => {
+                console.log(error);
+            })
+    }
 
-}, (error) =>  {
-console.log(error)
-})
-}
-deleteall() {
-this.restclient.post('api/deleteall',  {}).subscribe((result) =>  {
-console.log(result); 
-}, (error) =>  {
-console.log(error); 
-})
-}
+    getCurrentLoggedInUser() {
+        let email = (localStorage.getItem('userid'));
+        for (var i = 0; i < this.hospitals.length; i++) {
+            if (this.hospitals[i].email == email) {
+                this.hospital = this.hospitals[i];
+                this.activeHospitalId = this.hospitals[i]._id;
+                console.log(this.hospital);
+                return;
+            }
+        }
+    }
+
+    //post hospital data
+    posthospitaldata() {
+        console.log("hospital post");
+        this.restclient.post('api/posthospital', this.mockData).subscribe(
+            (result) => {
+                console.log("data is", result);
+
+            }, (error) => {
+                console.log(error)
+            })
+    }
+    deleteall() {
+        this.restclient.post('api/deleteall', {}).subscribe((result) => {
+            console.log(result);
+        }, (error) => {
+            console.log(error);
+        })
+    }
 }
 
