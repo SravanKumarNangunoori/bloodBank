bc080c7a5b588cef42359191a0ac8a1ce04f4660
SRAVAN KUMAR NANGUNOORI <nangunoori.sravan@gmail.com>
2018-11-15 16:01:59 -0500

    user updated hopspital emergency updated

--- src/app/hospital/hospital.component.ts
+++ src/app/hospital/hospital.component.ts
@@ -1,150 +1,183 @@
-import {Component, OnInit }from '@angular/core'; 
-import {RestClientService}from '../rest.client.service'; 
-import {HttpHeaderResponse }from '@angular/common/http'; 
-import {BrowserModule }from '@angular/platform-browser'; 
-import {NgModule }from '@angular/core'; 
-import {HttpModule, JsonpModule }from '@angular/http'; 
+import { Component, OnInit } from '@angular/core';
+import { RestClientService } from '../rest.client.service';
+
+import { } from '@types/googlemaps';
 
 
-@Component( {
-selector:'app-hospital', 
-templateUrl:'./hospital.component.html', 
-styleUrls:['./hospital.component.scss']
+@Component({
+    selector: 'app-hospital',
+    templateUrl: './hospital.component.html',
+    styleUrls: ['./hospital.component.scss']
 
 })
 export class HospitalComponent implements OnInit {
+    showEmergency: boolean;
+    emergencyNotification: any;
 
-constructor(private restclient:RestClientService) {}
-mockData:any; 
-getdata:any; 
-firstdata:string; 
-showForm:boolean; 
-hospitals:any; 
-hospital:any; 
-activeHospitalId:any; 
-showHospitalDetails:boolean; 
-sho:boolean=true;
-shwForm:boolean;
+    constructor(private restclient: RestClientService) { }
+    mockData: any;
+    getdata: any;
+    firstdata: string;
+    showForm: boolean;
+    hospitals: any;
+    hospital: any;
+    activeHospitalId: any;
+    showHospitalDetails: boolean;
+    sho: boolean = true;
+    shwForm: boolean;
+    temp: any;
+
+    ngOnInit() {
+        //this.mockData = { "Name": "Albany HOSPITAL","Address":"Albany","Apos":30,"Bpos":20,"ABpos":40,"Opos":25,"Aminus":30,"Bminus":20,"ABminus":40,"Ominus":25}
+        this.showForm = false;
+        this.showHospitalDetails = false;
+        this.showhospitaldata();
+        this.checkNotification();
+        this.temp = setInterval(() => {
+            this.checkNotification();
+        }, 5000);
+        this.showEmergency = true;
+    }
+    ngOnDestroy() {
+        clearInterval(this.temp);
+    }
 
 
-ngOnInit() {
-//this.mockData = { "Name": "Albany HOSPITAL","Address":"Albany","Apos":30,"Bpos":20,"ABpos":40,"Opos":25,"Aminus":30,"Bminus":20,"ABminus":40,"Ominus":25}
-this.showForm = false; 
-this.showHospitalDetails = false; 
-this.showhospitaldata(); 
-}
+    checkNotification() {
+        this.restclient.get('/api/getNotification').subscribe(
+            (result) => {
+                this.notification(result);
+            }
+        )
+    }
+    notification(result): any {
+        console.log(result);
 
-selectChange(event:any) {
-//this.showHospital(event.target.value);
-}
-selectDetailChange(event:any) {
-for (var i = 0; i < this.hospitals.length; i++) {
-if (this.hospitals[i]._id == event.target.value) {
-this.activeHospitalId = event.target.value; 
-return; 
-}
-}
-}
+        result.forEach(element => {
+            console.log(element);
+            if (element.hospital.email == localStorage.getItem('userid')) {
+                if (this.emergencyNotification != null) {
+                    if (this.emergencyNotification.user.email != element.user.email) {
+                        this.showEmergency = true;
+                    }
+                }
+                this.emergencyNotification = element;
+                // alert("Emergency at location"+element.location.lat + '  '+element.location.lon + 'by' + element.user.name);        
+            }
+        });
+    }
+    // selectChange(event: any) {
+    //     //this.showHospital(event.target.value);
+    // }
+    selectDetailChange(event: any) {
+        for (var i = 0; i < this.hospitals.length; i++) {
+            if (this.hospitals[i]._id == event.target.value) {
+                this.activeHospitalId = event.target.value;
+                return;
+            }
+        }
+    }
 
-showHospital() {
-this.showHospitalDetails = false; 
-let email = (localStorage.getItem('userid')); 
-for (var i = 0; i < this.hospitals.length; i++) {
-if (this.hospitals[i].email == email) {
-this.hospital = this.hospitals[i]; 
-this.activeHospitalId = this.hospitals[i]._id; 
-console.log(this.hospital); 
-return; 
-}
-}
-}
-// Update hospital data
-onFormSubmit(event) {
-var postadata = this.hospital; 
-this.restclient.post('api/updatehospital', postadata).subscribe((result) =>  {
-if (result) {this.showhospitaldata(); 
-}
-}, (error) =>  {
-console.log(error); 
-})
-}
-serializeForm(form) {
-var obj =  {}; 
-var elements = form.querySelectorAll("input, select, textarea"); 
-for (var i = 0; i < elements.length; ++i) {
-var element = elements[i]; 
-var name = element.name; 
-var value = element.value; 
+    showHospital() {
+        this.showHospitalDetails = false;
+        let email = (localStorage.getItem('userid'));
+        for (var i = 0; i < this.hospitals.length; i++) {
+            if (this.hospitals[i].email == email) {
+                this.hospital = this.hospitals[i];
+                this.activeHospitalId = this.hospitals[i]._id;
+                console.log(this.hospital);
+                return;
+            }
+        }
+    }
+    // Update hospital data
+    onFormSubmit(event) {
+        var postadata = this.hospital;
+        this.restclient.post('api/updatehospital', postadata).subscribe((result) => {
+            if (result) {
+                this.showhospitaldata();
+            }
+        }, (error) => {
+            console.log(error);
+        })
+    }
+    serializeForm(form) {
+        var obj = {};
+        var elements = form.querySelectorAll("input, select, textarea");
+        for (var i = 0; i < elements.length; ++i) {
+            var element = elements[i];
+            var name = element.name;
+            var value = element.value;
 
-if (name) {
-obj[ name ] = value; 
-}
-}
+            if (name) {
+                obj[name] = value;
+            }
+        }
 
-return obj; 
-}
-//get hospital data
-gethospitaldata() {
-    
-console.log("hospital"); 
-var that = this; 
-this.restclient.get('api/hospitalcollection').subscribe(
-(result) =>  {
-//var inp1 = document.getElementById("Name");
-//var inp2 = document.getElementById("Address");
-that.hospitals = result; 
-that.showHospital(); 
-that.showForm = this.sho; 
-that.shwForm=!this.sho;
-that.showHospitalDetails = false; 
-}, (error) =>  {
-console.log(error); 
-})
-}
+        return obj;
+    }
+    //get hospital data
+    gethospitaldata() {
 
-showhospitaldata() {
-var that = this; 
-this.restclient.get('api/hospitalcollection').subscribe(
-(result) =>  {
-that.hospitals = result; 
-that.showHospitalDetails = true; 
-that.showForm = false; 
-this.shwForm = true;
-that.getCurrentLoggedInUser(); 
-}, (error) =>  {
-console.log(error); 
-})
-}
+        console.log("hospital");
+        var that = this;
+        this.restclient.get('api/hospitalcollection').subscribe(
+            (result) => {
+                //var inp1 = document.getElementById("Name");
+                //var inp2 = document.getElementById("Address");
+                that.hospitals = result;
+                that.showHospital();
+                that.showForm = this.sho;
+                that.shwForm = !this.sho;
+                that.showHospitalDetails = false;
+            }, (error) => {
+                console.log(error);
+            })
+    }
 
-getCurrentLoggedInUser() {
-let email = (localStorage.getItem('userid')); 
-for (var i = 0; i < this.hospitals.length; i++) {
-if (this.hospitals[i].email == email) {
-this.hospital = this.hospitals[i]; 
-this.activeHospitalId = this.hospitals[i]._id; 
-console.log(this.hospital); 
-return; 
-}
-}
-}
+    showhospitaldata() {
+        var that = this;
+        this.restclient.get('api/hospitalcollection').subscribe(
+            (result) => {
+                that.hospitals = result;
+                that.showHospitalDetails = true;
+                that.showForm = false;
+                this.shwForm = true;
+                that.getCurrentLoggedInUser();
+            }, (error) => {
+                console.log(error);
+            })
+    }
 
-//post hospital data
-posthospitaldata() {
-console.log("hospital post"); 
-this.restclient.post('api/posthospital', this.mockData).subscribe(
-(result) =>  {
-console.log("data is", result); 
+    getCurrentLoggedInUser() {
+        let email = (localStorage.getItem('userid'));
+        for (var i = 0; i < this.hospitals.length; i++) {
+            if (this.hospitals[i].email == email) {
+                this.hospital = this.hospitals[i];
+                this.activeHospitalId = this.hospitals[i]._id;
+                console.log(this.hospital);
+                return;
+            }
+        }
+    }
 
-}, (error) =>  {
-console.log(error)
-})
-}
-deleteall() {
-this.restclient.post('api/deleteall',  {}).subscribe((result) =>  {
-console.log(result); 
-}, (error) =>  {
-console.log(error); 
-})
-}
+    //post hospital data
+    posthospitaldata() {
+        console.log("hospital post");
+        this.restclient.post('api/posthospital', this.mockData).subscribe(
+            (result) => {
+                console.log("data is", result);
+
+            }, (error) => {
+                console.log(error)
+            })
+    }
+    deleteall() {
+        this.restclient.post('api/deleteall', {}).subscribe((result) => {
+            console.log(result);
+        }, (error) => {
+            console.log(error);
+        })
+    }
 }
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RestClientService } from '../rest.client.service';
 import { DataShareService } from '../data.share.service';
-import { FormGroup ,FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-user',
@@ -13,10 +14,14 @@ export class UserComponent implements OnInit {
   mockData: any;
   userProfiles: {};
   currentUserProfile: any;
-  relativeModal:boolean;
-  sameBloodModal:boolean;
-  relativeForm:FormGroup;
-  emergencyForm:FormGroup;
+  relativeModal: boolean;
+  sameBloodModal: boolean;
+  relativeForm: FormGroup;
+  sameBloodForm: FormGroup;
+  hospitals: any;
+  lat: number;
+  lng: number;
+  nearestHospital: any;
 
   constructor(private restclient: RestClientService,
     private dataservice: DataShareService,
@@ -33,7 +38,7 @@ export class UserComponent implements OnInit {
       age: ['', Validators.required],
 
     });
-    this.emergencyForm = this.formBuilder.group({
+    this.sameBloodForm = this.formBuilder.group({
       email: ['', Validators.required],
       name: ['', Validators.required],
       phonenumber: ['', Validators.required],
@@ -46,11 +51,10 @@ export class UserComponent implements OnInit {
 
   ///get user data
   getUserprofile() {
-
     this.restclient.get('/api/usercollection').subscribe(
       (result) => {
         this.userProfiles = result;
-        console.log(this.userProfiles);
+        // console.log(this.userProfiles);
         this.getCurrentUserProfie();
       }, (error) => {
         console.log(error);
@@ -63,55 +67,124 @@ export class UserComponent implements OnInit {
     usrprofiles.forEach(element => {
       if (this.userData.email == element.email) {
         this.currentUserProfile = element;
-        console.log(this, this.currentUserProfile);
+        console.log("current user profile", this.currentUserProfile);
       }
 
     });
   }
   //post user Data
-  postUserData() {
-    this.restclient.post('/api/usercollection', this.mockData).subscribe(
-      (result) => {
-        console.log("Sucess");
-      }, (error) => {
-        console.log(error)
+  // postUserData() {
+  //   this.restclient.post('/api/usercollection', this.mockData).subscribe(
+  //     (result) => {
+  //       console.log("Sucess");
+  //     }, (error) => {
+  //       console.log(error)
+  //     }
+  //   )
+  // }
+
+  updateUserData() {
+    this.restclient.post('/api/updateUser', this.currentUserProfile).subscribe((result) => {
+      if (result) {
+        console.log(result);
+        this.getUserprofile();
       }
-    )
+    }, (error) => {
+      console.log(error);
+    })
   }
 
-  socialSignIn() {
-    this.restclient.post('api/socialSignIn()').subscribe(
-      (result) => {
-        console.log("Sucess");
-      }, (error) => {
-        console.log(error)
-      }
-    )
+  submitSameBloodForm(sameBloodForm) {
+    console.log(sameBloodForm);
+    this.currentUserProfile.sameblood.push(sameBloodForm);
+    this.updateUserData();
+    this.sameBloodModal = false;
   }
-  submitEmergencyForm(emergencyForm){
-console.log(emergencyForm);
-
-// let details={
-//   "email":this.currentUserProfile.email,
-//   "emergencyContact":emergencyForm
-// }
-// this.restclient.post('/api/postEmergency',details).subscribe(
-//   (result)=>{
-//     this.relativeModal=false;
-//     this.getUserprofile();
-//   },(error)=>{
-//     console.log(error);
-//   }
-// )
-  }
-  submitrelativeForm(relativeForm){
+  submitrelativeForm(relativeForm) {
     console.log(relativeForm);
+    this.currentUserProfile.relatives.push(relativeForm);
+    this.updateUserData();
+    this.relativeModal = false;
+  }
+
+  emergency() {
+    this.getLocationOfUser();
+    this.getAllHospitals();
+    var a,b;
+    setTimeout(() => {
+      
+    let nearesthospital=this.hospitals[0];
+   for(var i=0;i < this.hospitals.length - 1;i++){
+   a=this.calculateDistance(this.hospitals[i].location.latitutde,this.hospitals[i].location.longitude);
+   b= this.calculateDistance(this.hospitals[i+1].location.latitutde,this.hospitals[i+1].location.longitude);
+   console.log(a,b);
+    if(a<b){
+      nearesthospital=this.hospitals[i];
+    }
+   }
+   
+   this.nearestHospital=nearesthospital;
+   this.postNotification();
+    }, 1000);
+
+
+  }
+  getAllHospitals() {
+    this.restclient.get('api/hospitalcollection').subscribe(
+      (result) => {
+        this.hospitals = result;        
+      }, (error) => {
+        console.log("Error fetching hospital Details")
+      });
+  }
+
+  postNotification(){
+    let usernotify={
+      'user':this.currentUserProfile,
+      
+      'hospital':this.nearestHospital,
+      'location':{
+        'lat':this.lat,
+        'lon':this.lng
+      },
+    }
+    this.restclient.post('api/notify', usernotify).subscribe((result) => {
+      if (result) {
+        console.log(result);
+      }
+    }, (error) => {
+      console.log(error);
+    })
+  }
+  OpenRelativeModal() {
+    this.relativeModal = true;
+  }
+  OpenSameBLoodModal() {
+    this.sameBloodModal = true;
+  }
+  getLocationOfUser(){
+    if (navigator.geolocation) {
+      setTimeout('',1000);
+          navigator.geolocation.getCurrentPosition((geoSuccess) => {
+         console.log(geoSuccess);
+         this.lat=geoSuccess.coords.latitude;
+         this.lng=geoSuccess.coords.longitude;
+          },
+          (geoError)=>{
+             console.log(geoError);
+          });
+        } else {
+          alert("Geolocation is not supported by this browser.");
+        }
+  }
+
+  calculateDistance(lati,longi){
+    var distance=((this.lat -lati)*(this.lat -lati))+((this.lng-longi)*(this.lng-longi));
+    distance=Math.sqrt(distance);
+    console.log(distance);
+    return distance;
   }
   
-OpenEmergencyModal(){
-  this.relativeModal=true;
-}
-OpenSameBLoodModal(){
-  this.sameBloodModal=true;
-}
+
+
 }
